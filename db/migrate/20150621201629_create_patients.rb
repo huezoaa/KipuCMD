6d8cd4a89df980679b582ba65d634b7cdb6a9dcf
Angel Huezo <angelohuezo@gmail.com>
2015-06-21 16:16:45 -0400

    Initial commit

---
+++ db/migrate/20150621201629_create_patients.rb
@@ -0,0 +1,23 @@
+class CreatePatients < ActiveRecord::Migration
+  def change
+    create_table :patients do |t|
+      t.string :kipu_account_number
+      t.datetime :admission_date
+      t.string :first_name
+      t.string :last_name
+      t.string :middle_initial
+      t.string :gender
+      t.string :marital_status
+      t.string :diagnoses_codes
+      t.string :race
+      t.string :ethnicity
+      t.string :ssn
+      t.string :dob
+      t.string :phone
+      t.string :alt_phone
+      t.string :email
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ test/controllers/patients_controller_test.rb
@@ -0,0 +1,49 @@
+require 'test_helper'
+
+class PatientsControllerTest < ActionController::TestCase
+  setup do
+    @patient = patients(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:patients)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create patient" do
+    assert_difference('Patient.count') do
+      post :create, patient: { admission_date: @patient.admission_date, alt_phone: @patient.alt_phone, diagnoses_codes: @patient.diagnoses_codes, dob: @patient.dob, email: @patient.email, ethnicity: @patient.ethnicity, first_name: @patient.first_name, gender: @patient.gender, kipu_account_number: @patient.kipu_account_number, last_name: @patient.last_name, marital_status: @patient.marital_status, middle_initial: @patient.middle_initial, phone: @patient.phone, race: @patient.race, ssn: @patient.ssn }
+    end
+
+    assert_redirected_to patient_path(assigns(:patient))
+  end
+
+  test "should show patient" do
+    get :show, id: @patient
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get :edit, id: @patient
+    assert_response :success
+  end
+
+  test "should update patient" do
+    patch :update, id: @patient, patient: { admission_date: @patient.admission_date, alt_phone: @patient.alt_phone, diagnoses_codes: @patient.diagnoses_codes, dob: @patient.dob, email: @patient.email, ethnicity: @patient.ethnicity, first_name: @patient.first_name, gender: @patient.gender, kipu_account_number: @patient.kipu_account_number, last_name: @patient.last_name, marital_status: @patient.marital_status, middle_initial: @patient.middle_initial, phone: @patient.phone, race: @patient.race, ssn: @patient.ssn }
+    assert_redirected_to patient_path(assigns(:patient))
+  end
+
+  test "should destroy patient" do
+    assert_difference('Patient.count', -1) do
+      delete :destroy, id: @patient
+    end
+
+    assert_redirected_to patients_path
+  end
+end
