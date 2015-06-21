141433d5ff9ea1b598cbc9680947fd597056390b
Angel Huezo <angelohuezo@gmail.com>
2015-06-21 16:27:02 -0400

    Added seed file with Abe Lincoln. Will test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+patient01 = Patient.create(kipu_account_number: "100011001",
+                           admisssion_date: "20150615071514",
+                           first_name: "Abraham",
+                           last_name: "Lincoln",
+                           middle_initial: "H",
+                           gender: "M",
+                           marital_status: "M",
+                           diagnosis_codes: "301.75",
+                           race: "W",
+                           ethnicity: "N",
+                           ssn: "111112222",
+                           dob: "19000424",
+                           phone: "3055559999",
+                           alt_phone: "9543338888",
+                           email: "abe@lincoln.gov")
