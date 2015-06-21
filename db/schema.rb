0b7e0edaca193ff6609689c512026d0767df196f
Angel Huezo <angelohuezo@gmail.com>
2015-06-21 16:53:46 -0400

    ran migration to add Insurance table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621204607) do
+ActiveRecord::Schema.define(version: 20150621205237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20150621204607) do
     t.string   "zipcode"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "insurances", force: :cascade do |t|
+    t.string   "payment_method"
+    t.string   "company"
+    t.string   "policy_no"
+    t.string   "group_id"
+    t.datetime "created_at",     null: false
+    t.datetime "updated_at",     null: false
   end
 
   create_table "patients", force: :cascade do |t|
