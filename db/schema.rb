e51b01616a3cdfcfa2be13181d8ee39009fa3c78
Angel Huezo <angelohuezo@gmail.com>
2015-06-21 16:47:49 -0400

    Ran migration to add Addresses table

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150621201629) do
+ActiveRecord::Schema.define(version: 20150621204607) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string   "street_1"
+    t.string   "street_2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zipcode"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "patients", force: :cascade do |t|
     t.string   "kipu_account_number"
