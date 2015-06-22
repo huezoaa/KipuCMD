47014c425da05238dbb34597f787d73fff752342
Angel Huezo <angelohuezo@gmail.com>
2015-06-22 11:51:24 -0400

    Ran the migration files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622153535) do
+ActiveRecord::Schema.define(version: 20150622154526) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20150622153535) do
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
     t.integer  "patient_id"
+    t.string   "address_1"
+    t.string   "address_2"
+    t.string   "city"
+    t.string   "state"
+    t.string   "zipcode"
+    t.string   "phone"
   end
 
   add_index "insurances", ["patient_id"], name: "index_insurances_on_patient_id", using: :btree
